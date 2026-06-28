0880a2604eed7014cd32027533bb20d671e72e7b
Gabriel <gabrielconceicao09@gmail.com>
2026-06-28 18:38:10 -0300

    .

--- src/serial_imu_node.cpp
+++ src/serial_imu_node.cpp
@@ -149,7 +149,7 @@ class SerialImuNode : public rclcpp::Node
 
         if (valores.size() != 9)
         {
-            RCLCPP_INFO(this->get_logger(), "Leitura de número errado de valores");
+            RCLCPP_INFO(this->get_logger(), "Leitura de número errado de valores. Linha: %s", line.c_str());
             return; 
         }
         //----------------------------------------------------------------------------
