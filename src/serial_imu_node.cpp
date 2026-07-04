560241fdbb0ee366fe851feeca4a61e10e43ed19
Gabriel <gabrielconceicao09@gmail.com>
2026-07-03 23:40:20 -0300

    esqueleto mensagens twist pros encoders

--- src/serial_imu_node.cpp
+++ src/serial_imu_node.cpp
@@ -3,6 +3,7 @@
 #include "sensor_msgs/msg/nav_sat_fix.hpp"
 #include "sensor_msgs/msg/nav_sat_status.hpp"
 #include "sensor_msgs/msg/magnetic_field.hpp"
+#include "geometry_msgs/msg/twist_with_covariance_stamped.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
 #include "sensor_msgs/point_cloud2_iterator.hpp"
 #include "serial/serial.h"
@@ -37,8 +38,10 @@ class SerialImuNode : public rclcpp::Node
         variancia_gps_long_ = this->declare_parameter<double>("variancia_gps_long", 1.0);
         variancia_gps_lat_ = this->declare_parameter<double>("variancia_gps_lat", 1.0);
         variancia_gps_alt_ = this->declare_parameter<double>("variancia_gps_alt", 1.0);
-        offset_kalibr_s_ = this->declare_parameter<double>("offset_kalibr_s", -0.022073607573335652);
-        offset_kalibr = (int64_t) offset_kalibr_s_*1e9;
+        raio_roda_esq_ = this->declare_parameter<double>("raio_roda_esq", 0.125); //em metros
+        raio_roda_dir_ = this->declare_parameter<double>("raio_roda_dir", 0.125); //em metros 
+        //offset_kalibr_s_ = this->declare_parameter<double>("offset_kalibr_s", -0.022073607573335652);
+        //offset_kalibr = (int64_t) offset_kalibr_s_*1e9;
 
 
         raw_imu_topic_ = this->declare_parameter<std::string>("raw_imu_topic", "imu/data_raw");
@@ -106,6 +109,7 @@ class SerialImuNode : public rclcpp::Node
     int baudrate_;
     double variancia_acl_, variancia_gir_, variancia_mag_;
     double variancia_gps_long_, variancia_gps_lat_, variancia_gps_alt;
+    double raio_roda_esq_, raio_roda_dir_;
     //ConfigSonar sonar1_config_, sonar2_config_, sonar3_config_, sonar4_config_, sonar5_config_;
     //std::vector<ConfigSonar> sonares_;
 
@@ -121,12 +125,13 @@ class SerialImuNode : public rclcpp::Node
 
     //Variáveis para ajuste temporal das mensagens
     bool primeira_leitura = true;
-    int64_t offset_clocks_imu_ns;
-    double offset_kalibr_s_;
-    int64_t offset_kalibr;
+    int64_t offset_clocks_ns;
     double offset_alpha_ = 0.01;
-    int64_t ultimo_timestamp_imu_ns = 0;
-    uint32_t ultimo_micros_esp_imu, ultima_seq_imu;
+    //int64_t ultimo_timestamp_imu_ns = 0;
+    //double offset_kalibr_s_;
+    //int64_t offset_kalibr;
+    //uint32_t ultimo_micros_esp_imu, ultima_seq_imu;
+
 
     void ReadPub_callback()
     {
@@ -161,11 +166,11 @@ class SerialImuNode : public rclcpp::Node
         }
         catch (...)
         {
-            RCLCPP_WARN(this->get_logger(), "Erro na conversão de valores para double (IMU): %s", item.c_str());
+            RCLCPP_WARN(this->get_logger(), "Erro na conversão de valores para double: %s", item.c_str());
             return;
         }
 
-        if (valores.size() != 11)
+        if (valores.size() != 11) //TODO: CORRIGIR PARA NÚMERO CERTO DE VALORES
         {
             RCLCPP_INFO(this->get_logger(), "Leitura de número errado de valores. Linha: %s, n de valores: %i", line.c_str(), valores.size());
             return; 
@@ -188,24 +193,24 @@ class SerialImuNode : public rclcpp::Node
         rawImuMsg->linear_acceleration_covariance[0] = variancia_acl_;
         rawImuMsg->linear_acceleration_covariance[1] = 0.0;
         rawImuMsg->linear_acceleration_covariance[2] = 0.0;
-        rawImuMsg->linear_acceleration_covariance[3] = variancia_acl_;
-        rawImuMsg->linear_acceleration_covariance[4] = 0.0;
+        rawImuMsg->linear_acceleration_covariance[3] = 0.0;
+        rawImuMsg->linear_acceleration_covariance[4] = variancia_acl_;
         rawImuMsg->linear_acceleration_covariance[5] = 0.0;
-        rawImuMsg->linear_acceleration_covariance[6] = variancia_acl_;
+        rawImuMsg->linear_acceleration_covariance[6] = 0.0;
         rawImuMsg->linear_acceleration_covariance[7] = 0.0;
-        rawImuMsg->linear_acceleration_covariance[8] = 0.0;
+        rawImuMsg->linear_acceleration_covariance[8] = variancia_acl_;
         rawImuMsg->angular_velocity.x = valores[5];
         rawImuMsg->angular_velocity.y = valores[6];
         rawImuMsg->angular_velocity.z = valores[7];
         rawImuMsg->angular_velocity_covariance[0] = variancia_gir_;
         rawImuMsg->angular_velocity_covariance[1] = 0.0;
         rawImuMsg->angular_velocity_covariance[2] = 0.0;
-        rawImuMsg->angular_velocity_covariance[3] = variancia_gir_;
-        rawImuMsg->angular_velocity_covariance[4] = 0.0;
+        rawImuMsg->angular_velocity_covariance[3] = 0.0;
+        rawImuMsg->angular_velocity_covariance[4] = variancia_gir_;
         rawImuMsg->angular_velocity_covariance[5] = 0.0;
-        rawImuMsg->angular_velocity_covariance[6] = variancia_gir_;
+        rawImuMsg->angular_velocity_covariance[6] = 0.0;
         rawImuMsg->angular_velocity_covariance[7] = 0.0;
-        rawImuMsg->angular_velocity_covariance[8] = 0.0;
+        rawImuMsg->angular_velocity_covariance[8] = variancia_gir_;
 
         auto rawMagMsg = std::make_shared<sensor_msgs::msg::MagneticField>();
         rawMagMsg->header.frame_id = "imu_link";
@@ -215,24 +220,24 @@ class SerialImuNode : public rclcpp::Node
         rawMagMsg->magnetic_field_covariance[0] = variancia_mag_;
         rawMagMsg->magnetic_field_covariance[1] = 0.0;
         rawMagMsg->magnetic_field_covariance[2] = 0.0;
-        rawMagMsg->magnetic_field_covariance[3] = variancia_mag_;
-        rawMagMsg->magnetic_field_covariance[4] = 0.0;
+        rawMagMsg->magnetic_field_covariance[3] = 0.0;
+        rawMagMsg->magnetic_field_covariance[4] = variancia_mag_;
         rawMagMsg->magnetic_field_covariance[5] = 0.0;
-        rawMagMsg->magnetic_field_covariance[6] = variancia_mag_;
+        rawMagMsg->magnetic_field_covariance[6] = 0.0;
         rawMagMsg->magnetic_field_covariance[7] = 0.0;
-        rawMagMsg->magnetic_field_covariance[8] = 0.0;
+        rawMagMsg->magnetic_field_covariance[8] = variancia_mag_;
         
         //Composição do tempo de aquisição no relógio do sistema:
         if (primeira_leitura){
-            offset_clocks_imu_ns = tempo_ros.nanoseconds() - micros_esp_imu*1000LL;
+            offset_clocks_ns = tempo_ros.nanoseconds() - micros_esp_imu*1000LL;
             primeira_leitura = false;
         }
         else{
             //Ajuste contínuo do offset:
-            offset_clocks_imu_ns = (int64_t) offset_clocks_imu_ns*(1-offset_alpha_) + (tempo_ros.nanoseconds() - micros_esp_imu*1000LL)*offset_alpha_;
+            offset_clocks_ns = (int64_t) offset_clocks_ns*(1-offset_alpha_) + (tempo_ros.nanoseconds() - micros_esp_imu*1000LL)*offset_alpha_;
         }
 
-        int64_t timestamp_imu_ns = micros_esp_imu*1000LL + offset_clocks_imu_ns + offset_kalibr;
+        int64_t timestamp_imu_ns = micros_esp_imu*1000LL + offset_clocks_imu_ns; //+ offset_kalibr;
 
         //RCLCPP_INFO(this->get_logger(), "Dt: %li", timestamp_imu_ns-ultimo_timestamp_imu_ns);
 
@@ -258,7 +263,6 @@ class SerialImuNode : public rclcpp::Node
         
         /*//Composição da mensagem NavSatFix (GPS):
         auto gpsMsg = std::make_shared<sensor_msgs::msg::NavSatFix>();
-        gpsMsg->header.stamp = tempo_atual;
         gpsMsg->header.frame_id = "gps_link";
 
         //Status do gps:
@@ -273,6 +277,10 @@ class SerialImuNode : public rclcpp::Node
         gpsMsg->position_covariance = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0}; //Covariância das medidas do gps
         gpsMsg->position_covariance_type = 0;//COVARIANCE_TYPE_UNKNOWN;
 
+        int64_t micros_esp_gps = (int64_t) valore[] //TODO: Quando tiver parsing pronto, colocar índice correto.
+        int64_t timestamp_gps_ns = micros_esp_gps*1000LL + offset_clocks_ns;
+        gpsMsg->header.stamp = rclcpp::Time(timestamp_gps_ns);
+
         try {gps_pub_->publish(*gpsMsg);}
         catch (...) {RCLCPP_WARN(this->get_logger(), "Mensagem GPS não publicada");}
         */
@@ -281,7 +289,6 @@ class SerialImuNode : public rclcpp::Node
         //Composição da mensagem PointCloud2 (Sonar):
         auto sonarPc2Msg = std::make_shared<sensor_msgs::msg::PointCloud2>();
 
-        sonarPc2Msg->header.stamp = tempo_atual;
         sonarPc2Msg->header.frame_id = "base_link";
 
         //Modificador para alocação de memória e organização da mensagem PointCloud2
@@ -310,10 +317,51 @@ class SerialImuNode : public rclcpp::Node
 
             iter_x ++; iter_y ++; iter_z ++;
         }
+
+        int64_t micros_esp_sonares = (int64_t) valores[] //TODO: Quando tiver parsing pronto, colocar índice correto.
+        int64_t timestamp_sonares_ns = micros_esp_sonares*1000LL + offset_clocks_ns;
+        sonarPc2Msg->header.stamp = rclcpp::Time(timestamp_sonares_ns);
+
         try {sonar_pub_->publish(*sonarPc2Msg);}
         catch (...) {RCLCPP_WARN(this->get_logger(), "Mensagem sonares não publicada");}
         //--------------------------------------------------------------------------------
         */
+        /*
+        //Composição mensagem Twist dos encoders:
+
+        auto encodersMsg = std::make_shared<geometry_msgs::msg::TwistWithCovarianceStamped>();
+
+        encoderMsg->header.frame_id = "base_link";
+
+        encoderMsg->twist.twist.angular.x =;//TODO: inserir os valores lidos
+        encoderMsg->twist.twist.angular.y =;
+        encoderMsg->twist.twist.angular.z =;
+        encoderMsg->twist.twist.linear.x =;
+        encoderMsg->twist.twist.linear.y =;
+        encoderMsg->twist.twist.linear.z =;
+        encoderMsg->twist.covariance[0] =; //TODO: Variância dos encoders (acho que deve ser alta) 
+        encoderMsg->twist.covariance[1] =; encoderMsg->twist.covariance[2] =; encoderMsg->twist.covariance[3] =; 
+        encoderMsg->twist.covariance[4] =; encoderMsg->twist.covariance[5] =; encoderMsg->twist.covariance[6] =; 
+        encoderMsg->twist.covariance[7] =; 
+        encoderMsg->twist.covariance[8] =; encoderMsg->twist.covariance[9] =; encoderMsg->twist.covariance[10] =;
+        encoderMsg->twist.covariance[11] =; encoderMsg->twist.covariance[12] =; encoderMsg->twist.covariance[13] =;
+        encoderMsg->twist.covariance[14] =;
+        encoderMsg->twist.covariance[15] =; encoderMsg->twist.covariance[16] =; encoderMsg->twist.covariance[17] =;
+        encoderMsg->twist.covariance[18] =; encoderMsg->twist.covariance[19] =; encoderMsg->twist.covariance[20] =;
+        encoderMsg->twist.covariance[21] =;
+        encoderMsg->twist.covariance[22] =; encoderMsg->twist.covariance[23] =; encoderMsg->twist.covariance[24] =;
+        encoderMsg->twist.covariance[25] =; encoderMsg->twist.covariance[26] =; encoderMsg->twist.covariance[27] =;
+        encoderMsg->twist.covariance[28] =;
+        encoderMsg->twist.covariance[29] =; encoderMsg->twist.covariance[30] =; encoderMsg->twist.covariance[31] =;
+        encoderMsg->twist.covariance[32] =; encoderMsg->twist.covariance[33] =; encoderMsg->twist.covariance[34] =;
+        encoderMsg->twist.covariance[35] =;
+
+        int64_t micros_esp_encoders = (int64_t) valores[] //TODO: Quando tiver parsing pronto, colocar índice correto.
+        int64_t timestamp_encoders_ns = micros_esp_encoders*1000LL + offset_clocks_ns;
+        encodersMsg->header.stamp = rclcpp::Time(timestamp_encoders_ns);
+
+        */
+    
     }
 };
 
